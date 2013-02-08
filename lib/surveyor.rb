2ac72012399ac97c8eed6539b0e912e170afc299
Cedric Boussicaud <cedric.boussicaud@cisbeo.com>
2013-02-08 20:50:54 +0100

    simple_form

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -5,9 +5,10 @@ module Surveyor
 end
 require 'surveyor/common'
 require 'surveyor/acts_as_response'
+require 'simple_form'
 # require 'surveyor/surveyor_controller_methods'
 # require 'surveyor/models/survey_methods'
-require 'formtastic'
-Formtastic::FormBuilder.default_text_area_height = 5
-Formtastic::FormBuilder.default_text_area_width = 50
-Formtastic::FormBuilder.all_fields_required_by_default = false
+#require 'formtastic'
+#Formtastic::FormBuilder.default_text_area_height = 5
+#Formtastic::FormBuilder.default_text_area_width = 50
+#Formtastic::FormBuilder.all_fields_required_by_default = false
